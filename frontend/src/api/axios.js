525ebe094315b56fe4097bb0d89e9492e09ba14c
Amansurana2005 <amansurana5454@gmail.com>
2026-01-27 00:34:48 +0530

    frontend route change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://lab2market-backend.vercel.app/api", // backend URL
+  baseURL:
+    "https://lab2market-backend-eds75tv8f-aman-suranas-projects.vercel.app/api",
 });
 
 // Attach JWT token to every request if exists
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = token;
+  if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
 
